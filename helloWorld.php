11a04a5fb2818b1d0622d982cbb4ed261a20e419
Samson Royal <werefast2000@gmail.com>
2021-12-27 02:55:52 +0300

    adds php file

---
+++ helloWorld.php
@@ -0,0 +1,13 @@
+<!Doctype html>
+<html>
+    <head>
+        <title>Hello World</title>
+    </head>
+    <body>
+        <!-- This is the difference between a PHP and a HTML file -->
+        <?php
+            echo "Hello World"; 
+        ?>
+    </body>
+</html>
+<!-- But we still get the same results at the end of the day. -->

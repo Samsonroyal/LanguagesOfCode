e3efd6a6a7323874914b854bdbf0a3dbc57822e5
Samson Royal <werefast2000@gmail.com>
2021-12-27 01:46:10 +0300

    adds c sharp file

---
+++ helloWorld.cs
@@ -0,0 +1,8 @@
+namespace HelloWorldApp{
+    class HelloWorld{
+        static void Main(string[] args){
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
+// This is the end of the HelloWorldApp.cs file.
